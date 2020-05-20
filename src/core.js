e8140a700fd8d8c8c02d8293f9e0548a54fd5d79
Justin Baum <justin.baum@ninefx.com>
2020-05-19 20:35:11 -0400

    Vendor try 2

--- src/core.js
+++ src/core.js
@@ -8,8 +8,6 @@
 
 import * as shims from './shims';
 import { atob, btoa } from 'abab'
-import MD5 from './md5';
-import SHA1 from './sha1';
 import utils from './utils';
 
 /** Function: $build
@@ -1341,7 +1339,7 @@ Strophe.TimedHandler.prototype = {
  *
  *  This class is the main part of Strophe.  It manages a BOSH or websocket
  *  connection to an XMPP server and dispatches events to the user callbacks
- *  as data arrives. It supports SASL PLAIN, SASL SCRAM-SHA-1
+ *  as data arrives. It supports SASL PLAIN,
  *  and legacy authentication.
  *
  *  After creating a Strophe.Connection object, the user will typically
@@ -1409,7 +1407,6 @@ Strophe.TimedHandler.prototype = {
  *
  *      OAUTHBEARER - 60
  *      X-OAUTH2    - 50
- *      SCRAM-SHA-1  - 40
  *      PLAIN       - 30
  *      ANONYMOUS   - 20
  *      EXTERNAL    - 10
@@ -2270,8 +2267,7 @@ Strophe.Connection.prototype = {
             Strophe.SASLExternal,
             Strophe.SASLOAuthBearer,
             Strophe.SASLXOAuth2,
-            Strophe.SASLPlain,
-            Strophe.SASLSHA1
+            Strophe.SASLPlain
         ];
         mechanisms.forEach(this.registerSASLMechanism.bind(this));
     },
@@ -3166,7 +3162,6 @@ Strophe.Connection.prototype = {
  *
  *      OAUTHBEARER - 60
  *      X-OAUTH2    - 50
- *      SCRAM-SHA-1  - 40
  *      PLAIN       - 30
  *      ANONYMOUS   - 20
  *      EXTERNAL    - 10
@@ -3201,12 +3196,10 @@ Strophe.SASLMechanism = function(name, isClientFirst, priority) {
    *
    *  In the default configuration the priorities are
    *
-   *  SCRAM-SHA-1 - 40
    *  Plain - 20
    *
    *  Example: (This will cause Strophe to choose the mechanism that the server sent first)
    *
-   *  > Strophe.SASLPlain.priority = Strophe.SASLSHA1.priority;
    *
    *  See <SASL mechanisms> for a list of available mechanisms.
    *
@@ -3282,7 +3275,6 @@ Strophe.SASLMechanism.prototype = {
  *
  *  Strophe.SASLAnonymous   - SASL ANONYMOUS authentication.
  *  Strophe.SASLPlain       - SASL PLAIN authentication.
- *  Strophe.SASLSHA1        - SASL SCRAM-SHA-1 authentication
  *  Strophe.SASLOAuthBearer - SASL OAuth Bearer authentication
  *  Strophe.SASLExternal    - SASL EXTERNAL authentication
  *  Strophe.SASLXOAuth2     - SASL X-OAuth2 authentication
@@ -3318,85 +3310,6 @@ Strophe.SASLPlain.prototype.onChallenge = function(connection) {
     auth_str = auth_str + "\u0000";
     auth_str = auth_str + connection.pass;
     return utils.utf16to8(auth_str);
-};
-
-
-/** PrivateConstructor: SASLSHA1
- *  SASL SCRAM SHA 1 authentication.
- */
-Strophe.SASLSHA1 = function() {};
-Strophe.SASLSHA1.prototype = new Strophe.SASLMechanism("SCRAM-SHA-1", true, 40);
-
-Strophe.SASLSHA1.prototype.test = function(connection) {
-    return connection.authcid !== null;
-};
-
-Strophe.SASLSHA1.prototype.onChallenge = function(connection, challenge, test_cnonce) {
-    const cnonce = test_cnonce || MD5.hexdigest("" + (Math.random() * 1234567890));
-    let auth_str = "n=" + utils.utf16to8(connection.authcid);
-    auth_str += ",r=";
-    auth_str += cnonce;
-    connection._sasl_data.cnonce = cnonce;
-    connection._sasl_data["client-first-message-bare"] = auth_str;
-    auth_str = "n,," + auth_str;
-
-    this.onChallenge = (connection, challenge) => {
-        let nonce, salt, iter, Hi, U, U_old, i, k;
-        let responseText = "c=biws,";
-        let authMessage = `${connection._sasl_data["client-first-message-bare"]},${challenge},`;
-        const cnonce = connection._sasl_data.cnonce;
-        const attribMatch = /([a-z]+)=([^,]+)(,|$)/;
-
-        while (challenge.match(attribMatch)) {
-            const matches = challenge.match(attribMatch);
-            challenge = challenge.replace(matches[0], "");
-            switch (matches[1]) {
-            case "r":
-                nonce = matches[2];
-                break;
-            case "s":
-                salt = matches[2];
-                break;
-            case "i":
-                iter = matches[2];
-                break;
-            }
-        }
-
-        if (nonce.substr(0, cnonce.length) !== cnonce) {
-            connection._sasl_data = {};
-            return connection._sasl_failure_cb();
-        }
-
-        responseText += "r=" + nonce;
-        authMessage += responseText;
-
-        salt = atob(salt);
-        salt += "\x00\x00\x00\x01";
-
-        const pass = utils.utf16to8(connection.pass);
-        Hi = U_old = SHA1.core_hmac_sha1(pass, salt);
-        for (i=1; i<iter; i++) {
-            U = SHA1.core_hmac_sha1(pass, SHA1.binb2str(U_old));
-            for (k = 0; k < 5; k++) {
-                Hi[k] ^= U[k];
-            }
-            U_old = U;
-        }
-        Hi = SHA1.binb2str(Hi);
-
-        const clientKey = SHA1.core_hmac_sha1(Hi, "Client Key");
-        const serverKey = SHA1.str_hmac_sha1(Hi, "Server Key");
-        const clientSignature = SHA1.core_hmac_sha1(SHA1.str_sha1(SHA1.binb2str(clientKey)), authMessage);
-        connection._sasl_data["server-signature"] = SHA1.b64_hmac_sha1(serverKey, authMessage);
-
-        for (k = 0; k < 5; k++) {
-            clientKey[k] ^= clientSignature[k];
-        }
-        responseText += ",p=" + btoa(SHA1.binb2str(clientKey));
-        return responseText;
-    }
-    return auth_str;
 };
 
 
@@ -3467,18 +3380,12 @@ Strophe.SASLXOAuth2.prototype.onChallenge = function (connection) {
     return utils.utf16to8(auth_str);
 };
 
-export { Strophe, $build, $iq, $msg, $pres, SHA1, MD5 };
+export { Strophe, $build, $iq, $msg, $pres;
 
 export default {
     'Strophe':         Strophe,
     '$build':          $build,
     '$iq':             $iq,
     '$msg':            $msg,
-    '$pres':           $pres,
-    'SHA1':            SHA1,
-    'MD5':             MD5,
-    'b64_hmac_sha1':   SHA1.b64_hmac_sha1,
-    'b64_sha1':        SHA1.b64_sha1,
-    'str_hmac_sha1':   SHA1.str_hmac_sha1,
-    'str_sha1':        SHA1.str_sha1
+    '$pres':           $pres
 };
